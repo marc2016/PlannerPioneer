e27c1574fb45534e5dda2d800378c6e48ff3d814
Marc Lammers <marc@lammers.dev>
2026-02-08 17:43:24 +0100

    Tasks removed.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import Tasks from "./pages/Tasks";
 import Settings from "./pages/Settings";
 import "./App.css";
 
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="projects" element={<Projects />} />
-          <Route path="tasks" element={<Tasks />} />
           <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
--- src/pages/Tasks.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
-
-export default function Tasks() {
-    const { t } = useTranslation();
-    return (
-        <Typography variant="h4">{t('tasks.page_title')}</Typography>
-    );
-}
